4a8bea3561b7ccab8cff2468eb37eca38c3ef755
KhazAkar <damianzrb@zohomail.eu>
2025-03-31 20:10:15 +0200

    add few endpoints, track hits.

--- main.go
+++ main.go
@@ -1,29 +1,54 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"sync/atomic"
 )
 
-func healthHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
+type apiConfig struct {
+	fileserverHits atomic.Int32
 }
 
 func redirectHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/app/", http.StatusMovedPermanently)
 }
-
 func main() {
+	const filepathRoot = "./pages"
+	const port = "8080"
+
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/app", redirectHandler)
-	handler := http.FileServer(http.Dir("./pages"))
+	handler := http.FileServer(http.Dir(filepathRoot))
 	handler = http.StripPrefix("/app", handler)
-	mux.Handle("/app/", handler)
-	mux.HandleFunc("/healthz", healthHandler)
-	server := http.Server{
-		Addr:    ":8080",
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
 		Handler: mux,
 	}
-	server.ListenAndServe()
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
+}
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
